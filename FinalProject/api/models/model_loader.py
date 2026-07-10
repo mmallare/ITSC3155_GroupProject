d1597fdf5f389349d28f70e253c9806fc772cbad
Mike <mnmallare@gmail.com>
2026-07-10 11:05:51 -0400

    model loader updated

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, customers, ratings_reviews, payment_info, resource_management, promotions, menu_items, cart, statistics
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,11 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customers.Base.metadata.create_all(engine)
+    ratings_reviews.Base.metadata.create_all(engine)
+    payment_info.Base.metadata.create_all(engine)
+    resource_management.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    menu_items.Base.metadata.create_all(engine)
+    cart.Base.metadata.create_all(engine)
+    statistics.Base.metadata.create_all(engine)
