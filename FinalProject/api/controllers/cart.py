fdca1f70cf1f571fc90e661b5c1e878cd189baaf
Allan <allan.thavichith@gmail.com>
2026-07-20 21:06:12 -0400

    feat: Finished read_all and delete_cart pytest and they passed. Also cleaned up unused imports.

--- FinalProject/api/controllers/cart.py
+++ FinalProject/api/controllers/cart.py
@@ -1,6 +1,5 @@
-from httpx import request
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, status, Response, Depends
+from fastapi import HTTPException, status
 from ..models import cart as model
 from sqlalchemy.exc import SQLAlchemyError
 
--- FinalProject/api/tests/test_cart.py
+++ FinalProject/api/tests/test_cart.py
@@ -1,10 +1,8 @@
 from fastapi.testclient import TestClient
-from httpx import request
 from ..controllers import cart as controller
 from ..main import app
 import pytest
 from ..models import cart as model
-from ..schemas import cart as schema
 from decimal import Decimal
 
 client = TestClient(app)
@@ -33,6 +31,36 @@ def test_create_cart(db_session):
     assert created_cart.customer_id == 1
     assert created_cart.menu_item_id == 1
 
+def test_read_all(db_session):
+    cart_data = [
+        model.Cart(
+            id=1,
+            subtotal=Decimal("14.98"),
+            coupon="15OFF",
+            quantity=2,
+            customer_id=1,
+            menu_item_id=1
+        ),
+        model.Cart(
+            id=2,
+            subtotal=Decimal("19.98"),
+            coupon="10OFF",
+            quantity=1,
+            customer_id=2,
+            menu_item_id=3
+        )
+    ]
+    db_session.query.return_value.all.return_value = cart_data
+
+    result = controller.read_all(db=db_session)
+
+    assert result == cart_data
+    assert len(result) == len(cart_data)
+    assert result[0].id == 1
+    assert result[1].id == 2
+    assert result[0].subtotal == Decimal("14.98")
+    assert result[1].subtotal == Decimal("19.98")
+
 def test_read_one_cart(db_session):
     cart_data = model.Cart(
         id=1,
@@ -57,6 +85,24 @@ def test_read_one_cart(db_session):
     assert result.customer_id == 1
     assert result.menu_item_id == 1
 
+def test_delete_cart(db_session):
+    cart_data = model.Cart(
+        id=1,
+        subtotal=Decimal("14.98"),
+        coupon="15OFF",
+        quantity=2,
+        customer_id=1,
+        menu_item_id=1
+    )
+
+    # Mock the query used in the controller
+    cart_query = db_session.query.return_value.filter.return_value
+    cart_query.first.return_value = cart_data
+
+    # Call delete_cart function
+    deleted_cart = controller.delete_cart(db=db_session, cart_id=1)
+
+    assert deleted_cart is cart_data
 
 
 
