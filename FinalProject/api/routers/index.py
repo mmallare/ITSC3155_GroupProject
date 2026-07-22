38f348378f54f2ae66d023a95773fa3a7ffd20e9
Mike <mmallare@charlotte.edu>
2026-07-22 12:48:09 -0400

    router update

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,7 +1,10 @@
-from . import orders, order_details, payment_info
+from . import orders, order_details, payment_info, recipes, resources, sandwiches
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(payment_info.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
+    app.include_router(sandwiches.router)
